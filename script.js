4858c03b0b6eb71aafdf2552514156e8f41d1214
Marla Ingridh Rocha Guedes <marla.ingridh@gmail.com>
2022-09-09 17:07:59 -0400

    Iniciando desenvolvimento  - Jogo da Forca

---
+++ script.js
@@ -0,0 +1,7 @@
+let colecaoPalavras = ['ALURA','JAVASCRIPT','LOGICA','HTML','ORACLE','FORCA'];
+let tabuleiro = document.getElementById('forca').getContext('2d');
+
+function iniciaJogo(){
+    document.getElementById('botoes').style.display = 'none';
+    console.log(colecaoPalavras)
+}

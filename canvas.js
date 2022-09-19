561ed62f8167b9e35bfdd85b08caf53dc6b63ae7
Marla Ingridh Rocha Guedes <marla.ingridh@gmail.com>
2022-09-19 15:50:27 -0400

    Desenhando letras no canvas

---
+++ canvas.js
@@ -0,0 +1,54 @@
+function desenhaCanvas(){
+
+    tabuleiro.lineWidth = 8;
+    tabuleiro.lineCap = "round";
+    tabuleiro.lineJoin = "round";
+    tabuleiro.fillStyle = "#F3F5FC";
+    tabuleiro.strokeStyle = "#0A3871";
+
+    
+    //manipulação 
+    tabuleiro.fillRect(0,0,1200,800);
+    tabuleiro.beginPath();
+    tabuleiro.moveTo(700,500);
+    tabuleiro.lineTo(450,500);
+    tabuleiro.stroke(); 
+    tabuleiro.closePath();
+}
+
+
+function desenhaLinhas(){
+
+    tabuleiro.lineWidth = 6;
+    tabuleiro.lineCap = "round";
+    tabuleiro.lineJoin = "round";
+    tabuleiro.fillStyle = "#F3F5FC";
+    tabuleiro.strokeStyle = "#0A3871";
+
+    let largura = 600/palavraSecreta.length;
+
+    for(let i = 0; i < palavraSecreta.length; i++){
+        tabuleiro.moveTo(300+(largura*i), 640);
+        tabuleiro.lineTo(350+(largura*i), 640);
+    }
+
+    tabuleiro.stroke(); 
+    tabuleiro.closePath();
+
+}
+
+function escreveLetraCorreta(){
+    tabuleiro.font = '70px arial';
+    tabuleiro.lineWidth = 2;
+
+    
+    let largura = 600/palavraSecreta.length;
+
+    for(let i = 0; i < palavraSecreta.length; i++){
+        tabuleiro.strokeText(palavraSecreta[i],300+(largura*i),620);
+    }
+
+     
+
+}
+
--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-let colecaoPalavras = ['ALURA','JAVASCRIPT','LOGICA','HTML','ORACLE','FORCA'];
+
+let palavras = ['ALURA','JAVASCRIPT','LOGICA','HTML','ORACLE','FORCA'];
 let tabuleiro = document.getElementById('forca').getContext('2d');
+let palavraSecreta = "";
+
+function escolherPalavrasSecreta(){
+    let palavra = palavras[Math.floor(Math.random() * palavras.length)]; 
+    palavraSecreta = palavra;
+    console.log(palavraSecreta);
+
+    return palavraSecreta;
+}
+
+function validaValorDigitado(){
+    document.addEventListener('keyup',function(event){
+            var charCode = event.keyCode;
+       
+            if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123)){
+                return true;
+            }else{
+                alert('Digitar Letra!');
+                return false;
+            }
+    });    
+}
+
+function  comparaLetras(){
+
+
+}
+
 
 function iniciaJogo(){
     document.getElementById('botoes').style.display = 'none';
-    console.log(colecaoPalavras)
+    escolherPalavrasSecreta();
+    desenhaCanvas();
+    desenhaLinhas();
+    validaValorDigitado();
+    escreveLetraCorreta();
+    
 }
+
